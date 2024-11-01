74d40e502f661c00dc6940a841ac0b0a21602211
Rob-Yoo <jin12243@naver.com>
2024-11-01 19:17:40 +0900

    Chore: 모듈 생성 및 의존성 설정

---
+++ Data/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Jinyoung Yoo on 11/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
---
+++ Feature/Feature/Feature.h
@@ -0,0 +1,18 @@
+//
+//  Feature.h
+//  Feature
+//
+//  Created by Jinyoung Yoo on 11/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feature.
+FOUNDATION_EXPORT double FeatureVersionNumber;
+
+//! Project version string for Feature.
+FOUNDATION_EXPORT const unsigned char FeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feature/PublicHeader.h>
+
+

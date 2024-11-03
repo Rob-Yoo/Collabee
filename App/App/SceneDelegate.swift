7ad24fb3a76617b06a217e08731ed69c29f6ca97
Rob-Yoo <jin12243@naver.com>
2024-11-03 15:35:25 +0900

    Chore: KakaoAuth 관련 Xcode 환경 설정 및 예시 코드 작성

--- App/App/SceneDelegate.swift
+++ App/App/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import KakaoSDKUser
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +19,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+                    // 카카오 앱으로 로그인
+                    UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                        if let error = error {
+                            print(error)
+                        }
+                        else {
+                            // 카카오 계정으로 로그인
+                            print("loginWithKakaoTalk() success.")
+                            print(oauthToken)
+                        }
+                    }
+                }else {// 카카오톡 미설치 상태 -> 웹으로 이동해 로그인
+                    UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                            if let error = error {
+                                print(error)
+                            }
+                            else {
+                                print("loginWithKakaoAccount() success.")
+
+                                //do something
+                                _ = oauthToken
+                            }
+                        }
+                }
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

33e81e445ec0dd6c523f6608f0791f6d36dd33d7
Rob-Yoo <jin12243@naver.com>
2024-11-24 03:09:53 +0900

    Chore: 사라진 소스 파일 추가

---
+++ Authorization/Authorization/Sources/Data/Repository/Services/EmailAuthService.swift
@@ -0,0 +1,48 @@
+//
+//  EmailAuthService.swift
+//  Authorization
+//
+//  Created by Jinyoung Yoo on 11/24/24.
+//
+
+import Combine
+
+import DataSource
+import Common
+
+final class EmailAuthService: AuthService {
+    
+    @Injected private var networkProvider: NetworkProvider
+    private var cancellable = Set<AnyCancellable>()
+    
+    func perform() -> AnyPublisher<Void, AuthorizationError> {
+        
+        let loginBody = EmailLoginBody(email: "user@user.com", password: "Qwer1234!")
+        
+        return Future<Void, AuthorizationError> { [weak self] promise in
+            
+            guard let self else { return }
+            
+            networkProvider.request(UserAPI.emailLogin(loginBody), LoginResult.self, .withoutToken)
+                .sink { completion in
+                    switch completion {
+                    case .finished: break
+                    case .failure(let error):
+                        promise(.failure(.loginFailure))
+                    }
+                } receiveValue: { res in
+                    let token = res.token
+                    
+                    TokenStorage.save(token.accessToken, .access)
+                    TokenStorage.save(token.refreshToken, .refresh)
+                    promise(.success(()))
+                }
+                .store(in: &cancellable)
+            
+        }.eraseToAnyPublisher()
+        
+    }
+    
+    func handleOpenURL(_ url: URL) {}
+}
+

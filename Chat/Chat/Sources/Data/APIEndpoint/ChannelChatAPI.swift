1a2d2622b71823bc0462996fe222556a59c062c7
Rob-Yoo <jin12243@naver.com>
2024-11-28 19:51:19 +0900

    Feat: Chat 모듈 생성 및 API Enpoint 구성

---
+++ Chat/Chat/Sources/Data/APIEndpoint/ChannelChatAPI.swift
@@ -0,0 +1,69 @@
+//
+//  ChannelChatAPI.swift
+//  Chat
+//
+//  Created by Jinyoung Yoo on 11/28/24.
+//
+
+import DataSource
+
+struct ChatBody {
+    let content: String
+    let images: [Data]
+    
+    func makeMultipartFormData() -> [MultipartFormData] { return [] }
+}
+
+enum ChannelChatAPI {
+    case chatList(_ wsID: String, _ chID: String, _ cursorDate: Date)
+    case sendChat(_ wsID: String, _ chID: String, ChatBody)
+    case unreads(_ wsID: String, _ chID: String, _ after: Date)
+}
+
+extension ChannelChatAPI: API {
+    var path: String {
+        switch self {
+        case .chatList(let wsID, let chID, _):
+            return "/v1/workspaces/\(wsID)/channels/\(chID)/chats"
+        case .sendChat(let wsID, let chID, _):
+            return "/v1/workspaces/\(wsID)/channels/\(chID)/chats"
+        case .unreads(let wsID, let chID, _):
+            return "/v1/workspaces/\(wsID)/channels/\(chID)/unreads"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .chatList:
+            return .get
+        case .sendChat:
+            return .post
+        case .unreads:
+            return .get
+        }
+    }
+    
+    var task: HTTPTask {
+        switch self {
+        case .chatList(_, _, let cursorDate):
+            return .requestQueryParameters(parameters: ["cursor_date": cursorDate])
+        case .sendChat(_, _, let body):
+            return .uploadMultipart(body.makeMultipartFormData())
+        case .unreads(_, _, let after):
+            return .requestQueryParameters(parameters: ["after": after])
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .chatList:
+            return nil
+        case .sendChat:
+            return [
+                Header.contentType.rawValue: Header.multipart.rawValue
+            ]
+        case .unreads:
+            return nil
+        }
+    }
+}
---
+++ Chat/Chat/Sources/Data/APIEndpoint/DirectMessageAPI.swift
@@ -0,0 +1,93 @@
+//
+//  DirectMessageAPI.swift
+//  Chat
+//
+//  Created by Jinyoung Yoo on 11/28/24.
+//
+
+import DataSource
+
+struct EnterRoomBody: Encodable {
+    let opponentID: String
+    
+    enum CodingKeys: String, CodingKey {
+        case opponentID = "opponent_id"
+    }
+    
+    func makeMultiPartFormData() -> [MultipartFormData] { return [] }
+}
+
+enum DirectMessageAPI {
+    case enterRoom(_ wsID: String, EnterRoomBody)
+    case dmRooms(_ wsID: String)
+    case sendDM(_ wsID: String, _ roomID: String, ChatBody)
+    case dms(_ wsID: String, _ roomID: String, _ cursorDate: Date)
+    case unreads(_ wsID: String, _ roomID: String, _ after: Date)
+}
+
+extension DirectMessageAPI: API {
+    var path: String {
+        switch self {
+        case .enterRoom(let wsID, _):
+            return "/v1/workspaces/\(wsID)/dms"
+        case .dmRooms(let wsID):
+            return "/v1/workspaces/\(wsID)/dms"
+        case .sendDM(let wsID, let roomID, _):
+            return "/v1/workspaces/\(wsID)/dms/\(roomID)/chats"
+        case .dms(let wsID, let roomID, _):
+            return "/v1/workspaces/\(wsID)/dms/\(roomID)/chats"
+        case .unreads(let wsID, let roomID, _):
+            return "/v1/workspaces/\(wsID)/dms/\(roomID)/unreads"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .enterRoom:
+            return .post
+        case .dmRooms:
+            return .get
+        case .sendDM:
+            return .post
+        case .dms:
+            return .get
+        case .unreads:
+            return .get
+        }
+    }
+    
+    var task: HTTPTask {
+        switch self {
+        case .enterRoom(_, let enterRoomBody):
+            return .requestWithEncodableBody(enterRoomBody)
+        case .dmRooms:
+            return .requestPlain
+        case .sendDM(_, _, let chatBody):
+            return .uploadMultipart(chatBody.makeMultipartFormData())
+        case .dms(_, _, let cursorDate):
+            return .requestQueryParameters(parameters: ["cursor_date": cursorDate])
+        case .unreads(_, _, let after):
+            return .requestQueryParameters(parameters: ["after": after])
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .enterRoom:
+            return [
+                Header.contentType.rawValue: Header.json.rawValue
+            ]
+        case .dmRooms:
+            return nil
+        case .sendDM:
+            return [
+                Header.contentType.rawValue: Header.multipart.rawValue
+            ]
+        case .dms:
+            return nil
+        case .unreads:
+            return nil
+        }
+    }
+}
+
